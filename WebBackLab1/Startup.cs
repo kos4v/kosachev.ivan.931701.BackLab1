d455df10e161cad3af7f0174e3a72c6e31d07344
Иван Косачев <kosach_1@live.com>
2020-05-05 09:29:08 +0700

    Lab4_1

--- WebBackLab1/Startup.cs
+++ WebBackLab1/Startup.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,12 +17,13 @@ namespace WebBackLab1
         {
             Configuration = configuration;
         }
+
         public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
