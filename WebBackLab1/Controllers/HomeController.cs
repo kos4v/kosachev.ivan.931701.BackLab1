24f41c4cca20338b5bd7eb3e8c4d5a40736f7a2e
Иван Косачев <kosach_1@live.com>
2020-05-15 21:58:01 +0700

    FInal

--- WebBackLab1/Controllers/HomeController.cs
+++ WebBackLab1/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebBackLab1.Models;
 
@@ -12,25 +11,24 @@ namespace WebBackLab1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
     }
 }
---
+++ WebBackLab1/Models/AppdbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBackLab1.Models
+{
+    public class AppdbContext : DbContext
+    {
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+        public virtual DbSet<Lab> Labs { get; set; }
+        public virtual DbSet<Patient> Patients { get; set; }
+
+        public AppdbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=dbLab5;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.HasSequence<int>("Docid").IncrementsBy(1);
+            modelBuilder.HasSequence<int>("Labid").IncrementsBy(1);
+            modelBuilder.HasSequence<int>("Hosid").IncrementsBy(1);
+            modelBuilder.HasSequence<int>("Patid").IncrementsBy(1);
+
+            modelBuilder.Entity<Doctor>()
+                        .Property(o => o.Id)
+                        .HasDefaultValueSql("NEXT VALUE FOR Docid");
+
+            modelBuilder.Entity<Hospital>()
+                        .Property(o => o.Id)
+                        .HasDefaultValueSql("NEXT VALUE FOR Hosid");
+
+            modelBuilder.Entity<Lab>()
+                        .Property(o => o.Id)
+                        .HasDefaultValueSql("NEXT VALUE FOR Labid");
+
+            modelBuilder.Entity<Patient>()
+                        .Property(o => o.Id)
+                        .HasDefaultValueSql("NEXT VALUE FOR Patid");
+
+
+        }
+    }
+}
